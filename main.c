4180f0465cd1badaa6fdaeeb05f91d8201e0a01e
Greg Hurrell <greg@hurrell.net>
2024-03-28 22:09:55 +0100

    chore: update links

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 // main.c
-// Copyright 2011 Wincent Colaiuta. All rights reserved.
+// Copyright 2011-present Greg Hurrell. All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
